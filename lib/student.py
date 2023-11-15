45ab2673f5edd88dad5715afc0bca5cc5160ec46
rebalelaawar <rebalelaawar@gmail.com>
2023-11-15 16:00:12 -0500

    Inheritance Lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    knowledge = []
+
     
-    def learn(self):
-        pass
+    def learn(self,stri,knowledge=knowledge):
+        self.knowledge = knowledge
+        self.stri = stri
+        self.knowledge.append(self.stri)
+        
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,24 @@ from user import User
 
 import random
 
+
 class Teacher(User):
 
+    knowledge = ["str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",]
+
+   
+
+    def _init__(self, first_name, last_name, knowledge):
+        super().__init__(first_name, last_name)
+    
+
     def teach(self):
-        pass
+        return (self.knowledge[random.randint(0, len(self.knowledge)-1)])
+        
